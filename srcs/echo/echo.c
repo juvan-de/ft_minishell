881c5dca3a142e05799746c8408fe95fb3ddefae
Abel Van bergen en henegouwen <avan-ber@f0r3s2.codam.nl>
2020-10-19 19:09:24 +0200

    fixed echo

--- srcs/echo/echo.c
+++ srcs/echo/echo.c
@@ -6,14 +6,14 @@
 /*   By: avan-ber <avan-ber@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/19 17:51:24 by avan-ber      #+#    #+#                 */
-/*   Updated: 2020/10/19 18:35:27 by avan-ber      ########   odam.nl         */
+/*   Updated: 2020/10/19 19:06:24 by avan-ber      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 #include <stdbool.h>
 
-static	set_newline(void *data)
+static	set_newline(char *data)
 {
 	int i;
 
@@ -35,17 +35,20 @@ static	set_newline(void *data)
 		return (false);
 }
 
-void	ft_echo(tlist *list)
+void	ft_echo(t_list *list)
 {
 	bool	newline;
 
 	list = list->next;
-	newline = set_newline(list->data);
+	newline = set_newline(list->content);
 	if(newline == true)
 		list = list->next;
 	while(list->next)
-		ft_printf("%s ", (char*)list->data);
-	ft_printf("%s", (char*)list->data);
+	{
+		ft_printf("%s ", (char*)list->content);
+		list = list->next;
+	}
+	ft_printf("%s", (char*)list->content);
 	if(newline == true)
 		ft_printf("\n");
 }
